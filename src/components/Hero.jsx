8af32fa7b8e84627f3b975401946dc091622b1e4
hassancodess <dev.hassancodes@gmail.com>
2022-08-21 14:47:37 +0500

    fix some bugs

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,38 +4,40 @@ import Typed from 'react-typed'
 function Hero() {
   return (
     <>
-      {/* Hero Container */}
-      <div className='container p-4 py-32 mx-auto text-white bg-blue-900 xl:py-52 max-w-7xl'>
-        {/* Flex Container */}
-        <div className='flex flex-col items-center justify-center space-y-3'>
-          <h3 className='font-medium uppercase text-md text-cyanGreen md:text-xl'>
-            Growing with data Analytics
-          </h3>
-          <h1 className='text-3xl font-semibold tracking-wide md:text-4xl lg:text-5xl'>
-            Grow with data.
-          </h1>
-          <div className='flex flex-col items-center space-x-1 md:flex-row'>
-            <p className='text-xl font-semibold'>
-              Fast, flexible financing for{' '}
+      <section className='p-4 py-32 text-white xl:py-52'>
+        {/* Hero Container */}
+        <div className='container mx-auto max-w-7xl'>
+          {/* Flex Container */}
+          <div className='flex flex-col items-center justify-center space-y-3'>
+            <h3 className='font-medium uppercase text-md text-cyanGreen md:text-xl'>
+              Growing with data Analytics
+            </h3>
+            <h1 className='text-3xl font-semibold tracking-wide md:text-4xl lg:text-5xl'>
+              Grow with data.
+            </h1>
+            <div className='flex flex-col items-center space-x-1 md:flex-row'>
+              <p className='text-xl font-semibold'>
+                Fast, flexible financing for{' '}
+              </p>
+              <Typed
+                strings={['BTC', 'ETH', 'SASS']}
+                loop
+                typeSpeed={120}
+                backSpeed={120}
+                className='text-xl font-semibold text-cyanGreen'
+              />
+            </div>
+            <p className='max-w-lg text-lg font-semibold text-center text-gray-500 '>
+              Monitor your data analytics to increase revenue for BTB, ETH, &
+              SASS platforms
             </p>
-            <Typed
-              strings={['BTC', 'ETH', 'SASS']}
-              loop
-              typeSpeed={120}
-              backSpeed={120}
-              className='text-xl font-semibold text-cyanGreen'
-            />
+            {/* CTA */}
+            <button className='px-10 py-3 font-medium text-gray-900 rounded-md bg-cyanGreen'>
+              Get Started
+            </button>
           </div>
-          <p className='max-w-lg text-lg font-semibold text-center text-gray-500 '>
-            Monitor your data analytics to increase revenue for BTB, ETH, & SASS
-            platforms
-          </p>
-          {/* CTA */}
-          <button className='px-10 py-3 font-medium text-gray-900 rounded-md bg-cyanGreen'>
-            Get Started
-          </button>
         </div>
-      </div>
+      </section>
     </>
   )
 }
