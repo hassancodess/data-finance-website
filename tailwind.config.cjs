9080d8d4a56bbc8c460f1466f1808d41cf8c1c02
hassancodess <dev.hassancodes@gmail.com>
2022-08-21 12:27:04 +0500

    Add Colors & Fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Poppins', 'sans-serif'],
+    },
+    extend: {
+      colors: {
+        cyanGreen: '#00df9a',
+      },
+    },
   },
   plugins: [],
 }
